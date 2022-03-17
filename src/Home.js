9602779cf3b6991ab87c9f9162c9e514afa8571e
sbenedetti21 <sbenedetti@frba.utn.edu.ar>
2022-03-17 12:30:30 -0300

    banner responsive

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import './Home.css';
+import * as Icons from '@material-ui/icons';
 
 function Home() {
   return (
@@ -12,7 +13,14 @@ function Home() {
           </div>
         </div>
       </nav>
-
+      <div className='nav-buttons flex'>
+        <a href='https://instagram.com' target='_blank' rel="noreferrer"><Icons.Instagram/></a>
+        <a href='https://twitter.com' target='_blank' rel="noreferrer"><Icons.Twitter/></a>
+        <a href='https://solsea.io' target='_blank' rel="noreferrer"><Icons.ShoppingCart/></a>
+      </div>
+      <div id='banner-container'>
+        <img src={require('./assets/WalrusBar-home.jpg')}/>
+      </div>
     </div>
   );
 }
