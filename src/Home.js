2862214cee021f266f712e1914a2096aa53a234a
sbenedetti21 <sbenedetti@frba.utn.edu.ar>
2022-03-17 12:08:00 -0300

    logo and title

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './Home.css';
 
 function Home() {
   return (
-    <div className="Home">
-      inside home
+    <div className="common-container">
+      <nav id='home-nav' className='flex'>
+        <div className='flex'>
+          <img src={require('./assets/LogoBlanco.png')} alt='logo' id='home-logo'/>
+          <div className='flex home-title'>
+            <h1 id='thebig'>THE BIG</h1>
+            <h1 id='walrus'>WALRUS</h1>
+          </div>
+        </div>
+      </nav>
+
     </div>
   );
 }
