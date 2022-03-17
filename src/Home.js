97f11548a4327857802459d681d26e35be7ae2b1
sbenedetti21 <sbenedetti@frba.utn.edu.ar>
2022-03-17 12:37:00 -0300

    hover buttons

--- src/Home.js
+++ src/Home.js
@@ -7,16 +7,16 @@ function Home() {
       <nav id='home-nav' className='flex'>
         <div className='flex'>
           <img src={require('./assets/LogoBlanco.png')} alt='logo' id='home-logo'/>
-          <div className='flex home-title'>
+          <div className='flex home-title hover'>
             <h1 id='thebig'>THE BIG</h1>
             <h1 id='walrus'>WALRUS</h1>
           </div>
         </div>
       </nav>
       <div className='nav-buttons flex'>
-        <a href='https://instagram.com' target='_blank' rel="noreferrer"><Icons.Instagram/></a>
-        <a href='https://twitter.com' target='_blank' rel="noreferrer"><Icons.Twitter/></a>
-        <a href='https://solsea.io' target='_blank' rel="noreferrer"><Icons.ShoppingCart/></a>
+        <a href='https://instagram.com' target='_blank' rel="noreferrer" className='hover'><Icons.Instagram/></a>
+        <a href='https://twitter.com' target='_blank' rel="noreferrer" className='hover'><Icons.Twitter/></a>
+        <a href='https://solsea.io' target='_blank' rel="noreferrer" className='hover'><Icons.ShoppingCart/></a>
       </div>
       <div id='banner-container'>
         <img src={require('./assets/WalrusBar-home.jpg')}/>
