836e3b4ac65d050774d5ca46ceece60e174b7810
sbenedetti21 <sbenedetti@frba.utn.edu.ar>
2022-03-18 21:31:14 -0300

    first section and some of the second one

--- src/Bar.js
+++ src/Bar.js
@@ -36,6 +36,27 @@ function Bar () {
         <div className='banner-container'>
           <img src={require('./assets/insideWalrusBar-min.jpg')} alt='banner'/>
         </div>
+        <div id='below-banner'>
+          <h1>BUY A WALRUS</h1>
+          <p>The initial sale has sold out. To get your Walrus, check out the collection on OpenSea</p>
+          <a id='button-buy' href='https://opensea.io'>BUY A WALRUS ON OPENSEA</a>
+        </div>
+        <br/>
+        <div className='separator'/>
+        <div className='flex' id='second-section'>
+          <img src={require('./assets/photo_2022-03-18_21-21-42.jpg')} alt='walrus-02'/>
+          <div>
+            <h2>THE BIG</h2>
+            <h2 id='walrus'>WALRUS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus metus eu in eleifend. Tincidunt varius
+              ultricies consequat aliquam consequat in dolor maecenas. Risus egestas sit pharetra tortor. Suscipit arcu
+              nunc id fermentum tempus.<br />
+              Risus egestas sit pharetra tortor. Suscipit arcu nunc id fermentum. Risus egestas sit pharetra
+              tortor. Suscipit arcu nunc id fermentum tempus.Risus egestas.
+            </p>
+          </div>
+        </div>
+        <div className='separator'/>
       </motion.div>
   )
 }
