aa3528ab5c5590a203c072d01f27f280506218df
franciscoannoni <franciscoannoni@gmail.com>
2022-04-02 03:58:20 -0300

    agregos logos en el footer para arreglar

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,7 @@
 import './index.css';
 import NavButtons from "./NavButtons";
+import './Footer.css'
+
 
 function Footer() {
   return(
@@ -8,11 +10,17 @@ function Footer() {
           <h2>Join the developer list</h2>
           <p id='sub'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus metus eu in eleifend. Tincidunt varius ultricies consequat aliquam consequat in dolor maecenas.</p>
         </div>
+        <div id='logos'>
+        <img src={require('./assets/LogoBlanco.png')}  width="40"
+     height="31"/>
+        <img src={require('./assets/Solana.png')} />
+        <img src={require('./assets/OpenSea.png')}/>
+        </div>
         <div>
           <div className='footer-buttons flex'>
             <NavButtons />
           </div>
-          <p id='rights'>2022 TheBigWalrus<br/>Todos los derechos reservados</p>
+          <p id='rights'>@2021 TBW.TheBigWalrus<br/>All Rights Reserved.</p>
         </div>
       </footer>
   )
