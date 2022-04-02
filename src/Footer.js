6aed6bedf29e5bc41106cfed80776409ce6640d0
franciscoannoni <franciscoannoni@gmail.com>
2022-04-02 04:08:11 -0300

    fakta arreglar bien el footer y el roadmap a texto

--- src/Footer.js
+++ src/Footer.js
@@ -11,10 +11,15 @@ function Footer() {
           <p id='sub'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus metus eu in eleifend. Tincidunt varius ultricies consequat aliquam consequat in dolor maecenas.</p>
         </div>
         <div id='logos'>
-        <img src={require('./assets/LogoBlanco.png')}  width="40"
-     height="31"/>
-        <img src={require('./assets/Solana.png')} />
-        <img src={require('./assets/OpenSea.png')}/>
+        <a href='https://opensea.io/collection/thebigwalerus-store/'  target='_blank' rel="noreferrer">
+          <img src={require('./assets/LogoBlanco.png')}  width="40" height="31" />
+        </a>
+        <a href='https://solana.com/ecosystem/'  target='_blank' rel="noreferrer">
+          <img src={require('./assets/Solana.png')}  />
+        </a>
+        <a  href='https://opensea.io/' target='_blank' rel="noreferrer">
+          <img src={require('./assets/OpenSea.png')}/>
+        </a>
         </div>
         <div>
           <div className='footer-buttons flex'>
